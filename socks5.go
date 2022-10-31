83a02deb60f11eb2479e3a6462b9d685b8101a41
whatwerwant <tobewhatwewant@outlook.com>
2022-10-31 10:00:19 +0800

    chore: update

---
+++ socks5.go
@@ -0,0 +1,5 @@
+package socks5
+
+// References:
+//   [wzshiming/socks5](https://github.com/wzshiming/socks5) - Socks5/Socks5h server and client. Full TCP/Bind/UDP and IPv4/IPv6 support
+//   socks5协议中的BIND与UDP ASSOCIATE - https://www.jianshu.com/p/55c0259d1a36
